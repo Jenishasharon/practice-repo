84c06a80d331e38113aac8e8f2146c81535b1e6d
Jenisha Sharon <Jenisha Sharon@DESKTOP-08V9D63>
2024-07-25 18:12:41 +0530

    constructor example

---
+++ practiceproject/src/main/java/practiceproject1/ConstructorExample2.java
@@ -0,0 +1,22 @@
+package practiceproject1;
+
+public class ConstructorExample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ConstructorExample2 obj=new ConstructorExample2();
+ConstructorExample2.data();
+	}
+public ConstructorExample2()
+{
+	int a=15;
+	int b=10;
+	int c=a+b;
+	System.out.println(c);
+}
+public static void data()
+{
+	String name="jenisha";
+	System.out.println(name);
+}
+}
